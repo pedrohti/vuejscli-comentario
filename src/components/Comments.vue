05834e7506fdc3a5cd06d329eb0b83780ab37d03
Pedro <pedroh.ti@hotmail.com>
2020-02-20 21:47:06 -0300

    Comentários com VueJS-CLI

---
+++ src/components/Comments.vue
@@ -0,0 +1,55 @@
+<template>
+	<div class="container">
+		<h1>Comentários</h1>
+		<hr />
+		<FormTodo v-on:add-todo="addComment" />
+		<div class="list-group">
+			<p v-if="comments.length <= 0">Sem comentários...</p>
+			<div class="list-group-item" v-for="(comment, index) in allComments" v-bind:key="index">
+				<span class="comment__author">Autor: <strong>{{comment.name}}</strong></span>
+				<p>{{comment.message}}</p>
+				<div>
+					<a v-on:click.prevent="removeComment(index)" href="#" title="Excluir">Excluir</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import FormTodo from './FormTodo'
+
+export default {
+	components: {
+		FormTodo
+	},
+	data() {
+		return {
+			comments: [],
+		}
+	},
+	methods: {
+		addComment(comment){
+			this.comments.push(comment)
+
+		},
+		removeComment(index) {
+			this.comments.splice(index, 1)
+		}
+	},
+	computed: {
+		allComments() {
+			return this.comments.map(comment => ({
+				...comment,
+				name: comment.name.trim() === '' ? 'Anônimo' : comment.name
+			}))
+		}
+	},
+	watch: {
+		comments(val) {
+			console.log('val', val)
+		}
+	}
+
+}
+</script>
